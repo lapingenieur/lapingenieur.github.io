db5666df31125a43c1c78a2d479d78c10a8b2838
lapingenieur <ep.elio.poletti@gmail.com>
2020-12-04 20:37:27 +0100

    menu is foreground and no hover pb w/ content

--- compute/index_nav.js
+++ compute/index_nav.js
@@ -1,13 +1,18 @@
-var locked = document.getElementById('locked');
-var lockedTop = locked.offsetTop;
+var menu = document.getElementById('menu');
+var menuTop = menu.offsetTop;
+var nav = document.getElementById('nav');
+var navTop = nav.offsetTop;
 
 window.onscroll = function () { myScrollFunction() };
 
 function myScrollFunction() {
-var res = lockedTop - document.documentElement.scrollTop;
+var res = menuTop - document.documentElement.scrollTop;
+var navme = res + 32;
     if (res > 0) {
-        locked.setAttribute('style', 'top:' + res + 'px');
+        menu.setAttribute('style', 'top:' + res + 'px');
+        nav.setAttribute('style', 'top:' + navme + 'px');
     } else {
-        locked.setAttribute('style', 'top:0px')
+        menu.setAttribute('style', 'top:0px')
+        nav.setAttribute('style', 'top:32px')
     }
 }
